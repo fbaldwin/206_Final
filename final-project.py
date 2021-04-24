ede0f9ed2e1598f952ed171d4fd97e1294ae557e
Freddie Baldwin <fbaldwin@umich.edu>
2021-04-24 17:30:40 -0400

    finished getMovieNames

--- final-project.py
+++ final-project.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import sqlite3
 import os
 import matplotlib.pyplot as plt
+import re
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -14,13 +15,32 @@ def setUpDatabase(db_name):
 #return a list of tuples in the format (year,title) for first ten movies of that year
 
 def getMovieNames(year):
-    url = 'filmsite.org/' + str(year) + ".html"
+    url = 'https://filmsite.org/' + str(year) + ".html"
     r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    anchor = soup.find_all('p')
+    x = []
+    reg = r'\b(.+)\('
+    for i in anchor:
+        try:
+            orig = i.find('b').text
+        except:
+            continue
+        y = re.findall(reg, orig)
+        for j in y:
+            x.append(j[:-1])
+    new_lst = x[1:11]
+    tuple_lst = []
+    for i in new_lst:
+        tuple_lst.append((year, i))
 
-    pass
+    return tuple_lst
 
 
 #call getMovieNames for each year dating back to 1919 to add all of the movie titles and years to the table movies
 
 def moviesToDatabase(url):
     pass
+
+
+print(getMovieNames(2020))
